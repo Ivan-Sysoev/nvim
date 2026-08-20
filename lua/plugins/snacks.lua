e49cdb9030e698d34cbba14a2fb05ee308a4d5cd
Ivan-Sysoev <ivansysoev007@gmail.com>
2026-08-21 00:11:11 +0300

    Remapped notification history to <leader>nh. Configured notifications: (only warnings and errors pop ups)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,7 +1,18 @@
 return {
 	"folke/snacks.nvim",
+	keys = {
+		{ "<leader>n", false },
+		{
+			"<leader>nh",
+			function()
+				Snacks.picker.notifications()
+			end,
+			desc = "Notification History",
+		},
+	},
 	opts = {
 		dashboard = { enabled = false }, -- the session restores itself, no start screen needed
+		notifier = { level = vim.log.levels.WARN }, -- only warnings and errors pop up
 		explorer = {
 			enabled = false,
 			trash = true, -- Use the system trash when deleting files
